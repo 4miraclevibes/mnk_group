53ac7ded260d8a8532d4727d1ecb691651dbca1c
justputoff <kholid.hasibuan75@gmail.com>
2026-08-10 13:08:49 +0700

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Bimbel Jagratara</title>
+        <title>Bimbel KOKO</title>
         <link rel="icon" type="image/x-icon" href="{{ asset('mnk_logo-rbg.png') }}" style="max-width: 250px; transform: rotate(270deg);"  />
 
         <!-- Fonts -->
